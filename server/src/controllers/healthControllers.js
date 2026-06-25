1acfed338969a67a54c5b09df73e09ce7e7a706b
Manojna <bantumanojna@gmail.com>
2026-06-25 22:40:42 +0530

    Add health controller and about endpoint

--- server/src/controllers/healthControllers.js
+++ server/src/controllers/healthControllers.js
@@ -4,7 +4,15 @@ const getHealthStatus = (req, res) => {
         message: "InterviewHub API is running"
     });
 };
+const getAboutInfo = (req, res) => {
+    res.json({
+        project: "InterviewHub",
+        version: "1.0.0",
+        developer: "Manojna"
+    });
+};
 
 module.exports = {
-    getHealthStatus
+    getHealthStatus,
+    getAboutInfo
 };
--- server/src/routes/healthRoutes.js
+++ server/src/routes/healthRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getHealthStatus
-} = require("../controllers/healthController");
+    getHealthStatus,getAboutInfo
+} = require("../controllers/healthControllers");
 
 router.get("/health", getHealthStatus);
+router.get("/about",getAboutInfo);
 
 module.exports = router;
+
