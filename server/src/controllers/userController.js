3cbc696863d44e7a654e7a94c858ea669ac93513
Manojna <bantumanojna@gmail.com>
2026-07-04 20:23:52 +0530

    Add JWT authentication middleware and protected profile API

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -112,6 +112,27 @@ const loginUser=async(req,res)=>{
         });
     }
 }
+const getProfile = async (req, res) => {
+    try {
+        const userId=req.user.userId;
+        const user=await User.findById(userId).select("-password -__v");
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            });
+        }return res.status(200).json({
+            success:true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error"
+        });
+    }
+}
 module.exports = {
-    createUser,loginUser
+    createUser,loginUser,getProfile
 };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -8,9 +8,10 @@ module.exports=router;*/
 const express = require("express");
 const router = express.Router();
 
-const { createUser, loginUser } = require("../controllers/userController");
+const { createUser, loginUser,getProfile } = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/users", createUser);
 router.post("/users/login", loginUser);
-
+router.get("/users/profile",authMiddleware,getProfile);
 module.exports = router;
